e8ac98980076502ac755d0a7af98448dfebcb56f
wael <waeelash155@gmail.com>
2022-04-06 10:01:22 -0400

    commit tree f77746cc616bbcedd99e47107405957137bd5b15

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    "The weather outside is frightful.".sentence?
   end
 
   def question?
-
+    "What time is it?".question?
   end
 
   def exclamation?
-
+    "I am so excited!".exclamation?
   end
 
   def count_sentences
-
+    "one. two. three?".count_sentences
   end
 end
